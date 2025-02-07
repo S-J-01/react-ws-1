0408f67aa673c4fbbad8fa515d2dcc656fb8853f
Shreyas Jaltare <shreyasjaltare@gmail.com>
2025-02-07 22:27:45 +0530

    FE connection for ws server

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
+import { useActionState, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setSocket] = useState<WebSocket | null>(null)
+  const [Latestmessage, setLatestMessage] = useState("")
+
+  useEffect(()=>{
+    const socket = new WebSocket('ws://localhost:8080')
+    
+
+    socket.onopen = ()=>{
+      console.log('connected')
+      setSocket(socket)
+    }
+
+    socket.onmessage = (message)=>{
+      console.log(message.data)
+      setLatestMessage(message.data)
+    }
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <button onClick={()=>{
+      socket?.send("Hello")
+     }}>Say Hello</button>
+      {Latestmessage}
+       
     </>
   )
 }
